60cd19a525983f19072375228680d8f9505a32f7
john.tam <john.tam@technine.io>
2024-09-30 12:14:37 +0800

    update dist folder

--- dist/lib/helper.js
+++ dist/lib/helper.js
@@ -1,24 +1,23 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.createFieldAlias = exports.createColumns = exports.createOrderBy = exports.createWhereCondition = void 0;
+exports.createWhereCondition = createWhereCondition;
+exports.createOrderBy = createOrderBy;
+exports.createColumns = createColumns;
+exports.createFieldAlias = createFieldAlias;
 // Helper function to create a WhereCondition
 function createWhereCondition(conditions) {
     return conditions;
 }
-exports.createWhereCondition = createWhereCondition;
 // Helper function to create an orderBy array
 function createOrderBy(fields) {
     return fields;
 }
-exports.createOrderBy = createOrderBy;
 // Helper function to create columns
 function createColumns(columns) {
     return columns;
 }
-exports.createColumns = createColumns;
 // Helper function to create field alias
 function createFieldAlias(fieldObj) {
     return fieldObj;
 }
-exports.createFieldAlias = createFieldAlias;
 //# sourceMappingURL=helper.js.map
